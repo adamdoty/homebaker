85912a487db9e58b6d8b286b4a72e384e1dfd5e5
Adam Doty <acdoty15@gmail.com>
2022-09-14 17:16:12 -0700

    created Treat and Note models; created a superuser; added Treat Model to admin site

--- homebaker/settings.py
+++ homebaker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'treats.apps.TreatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ treats/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Treat(models.Model):
+    name = models.CharField(max_length=250)
+    recipe_link = models.URLField()
+    picture = models.ImageField()
+
+
+class Note(models.Model):
+    treat = models.ForeignKey(Treat, on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+# class Comment(models.Model):
+#     treat = models.ForeignKey(Treat, on_delete=models.CASCADE)
+#     body = models.TextField()
+#     publish = models.DateTimeField(default=timezone.now)
+#     created = models.DateTimeField(auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#
+#
+# class Review(models.Model):
+#     treat = models.ForeignKey(Treat, on_delete=models.CASCADE)
+#     body = models.TextField()
+#     rating = models.IntegerField()
+#     publish = models.DateTimeField(default=timezone.now)
+#     created = models.DateTimeField(auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
