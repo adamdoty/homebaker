9d21787b565fbe75bb0804de04dfcf4f79cd862a
Bob Belderbos <bobbelderbos@gmail.com>
2022-09-22 17:58:19 +0200

    got aws file upload working

--- treats/aws.py
+++ treats/aws.py
@@ -4,29 +4,22 @@ Used to interface with aws s3 bucket for image storage."""
 import argparse
 import os
 from typing import Optional
+from django.conf import settings
 
 import boto3
-from dotenv import load_dotenv
-
-DEFAULT_BUCKET_PERMISSION = "public-read"
-
-load_dotenv()
 
 
-def upload_to_s3(
-        filepath: str, bucket: Optional[str] = None, acl: Optional[str] = None
-) -> str:
-    s3_bucket = bucket or os.environ["AWS_S3_BUCKET"]
-    acl = acl or DEFAULT_BUCKET_PERMISSION
-
-    aws_key = os.environ["AWS_ACCESS_KEY_ID"]
-    aws_secret = os.environ["AWS_SECRET_ACCESS_KEY"]
-    aws_region = os.environ["AWS_REGION"]
+def upload_to_s3(file, acl="public-read"):
+    s3_bucket = settings.AWS_STORAGE_BUCKET_NAME
+    aws_key = settings.AWS_ACCESS_KEY_ID
+    aws_secret = settings.AWS_SECRET_ACCESS_KEY
+    aws_region = settings.AWS_S3_REGION_NAME
 
     session = boto3.Session(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
+
     s3 = session.resource("s3")
     response = s3.Bucket(s3_bucket).put_object(
-        Key=os.path.basename(filepath), Body=open(filepath, "rb"), ACL=acl
+        Key=file.name, Body=file.read(), ACL=acl
     )
 
     s3_file_link = f"https://{s3_bucket}.s3.{aws_region}.amazonaws.com/{response.key}"
--- treats/forms.py
+++ treats/forms.py
@@ -4,7 +4,9 @@ from .models import Treat
 
 
 class TreatForm(forms.ModelForm):
+    img_upload = forms.FileField(required=False, label="Upload an image")
+
     class Meta:
         model = Treat
-        fields = ['title', 'description', 'cover_img', 'rating']
+        fields = ['title', 'description', 'img_upload', 'rating']
         labels = {'title': 'title', 'description': 'description', 'cover_img': 'cover_img', 'rating': 'rating'}
