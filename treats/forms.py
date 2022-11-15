b610ab18178a18cd07615d6b15ccbbcef15b88cc
Adam Doty <acdoty15@gmail.com>
2022-11-15 10:49:25 -0800

    added docs to gitignore

--- treats/forms.py
+++ treats/forms.py
@@ -9,47 +9,64 @@ class TreatForm(ModelForm):
 
     class Meta:
         model = Treat
-        fields = ['title', 'description', 'img_upload', 'recipe_source']  # 'rating',
-        labels = {'title': 'Title', 'description': 'Description', 'cover_img': 'Cover_img',
-                  'recipe_source': 'Recipe source'}  # 'rating': 'Rating',
+        fields = ["title", "description", "img_upload", "recipe_source"]  # 'rating',
+        labels = {
+            "title": "Title",
+            "description": "Description",
+            "cover_img": "Cover_img",
+            "recipe_source": "Recipe source",
+        }  # 'rating': 'Rating',
 
 
 class NoteForm(ModelForm):
     class Meta:
         model = Note
-        fields = ['body']
-        labels = {'body': 'Note content'}
+        fields = ["body"]
+        labels = {"body": "Note content"}
 
 
 class CouponForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CouponForm, self).__init__(*args, **kwargs)
-        self.fields['recipient'].queryset = User.objects.filter(profile__is_baker_user=False)
-        self.fields['treat'].queryset = Treat.objects.filter(is_recipient_request=False)
+        self.fields["recipient"].queryset = User.objects.filter(
+            profile__is_baker_user=False
+        )
+        self.fields["treat"].queryset = Treat.objects.filter(is_recipient_request=False)
 
     class Meta:
         model = Coupon
-        fields = ['reason', 'recipient', 'treat', 'target_date', 'expiration_date']
-        labels = {'reason': 'Reason for Coupon', 'recipient': 'Coupon Recipient', 'treat': 'Treat this coupon is for',
-                  'target_date': 'Date of Event/Coupon Reason', 'expiration_date': 'Expiration Date'}
+        fields = ["reason", "recipient", "treat", "target_date", "expiration_date"]
+        labels = {
+            "reason": "Reason for Coupon",
+            "recipient": "Coupon Recipient",
+            "treat": "Treat this coupon is for",
+            "target_date": "Date of Event/Coupon Reason",
+            "expiration_date": "Expiration Date",
+        }
         widgets = {
-            'target_date': DateInput(
-                format='%Y-%m-%d',
-                attrs={'class': 'form-control',
-                       'placeholder': 'Select a date',
-                       'type': 'date'}),
-            'expiration_date': DateInput(
-                format='%Y-%m-%d',
-                attrs={'class': 'form-control',
-                       'placeholder': 'Select a date',
-                       'type': 'date'}),
+            "target_date": DateInput(
+                format="%Y-%m-%d",
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Select a date",
+                    "type": "date",
+                },
+            ),
+            "expiration_date": DateInput(
+                format="%Y-%m-%d",
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Select a date",
+                    "type": "date",
+                },
+            ),
         }
 
 
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ('is_baker_user',)
+        fields = ("is_baker_user",)
 
 
 class TreatRequestForm(ModelForm):
@@ -57,15 +74,24 @@ class TreatRequestForm(ModelForm):
 
     class Meta:
         model = Treat
-        fields = ['title', 'description', 'recipe_source']
-        labels = {'title': 'Title', 'description': 'Description', 'recipe_source': 'Recipe Source'}
+        fields = ["title", "description", "recipe_source"]
+        labels = {
+            "title": "Title",
+            "description": "Description",
+            "recipe_source": "Recipe Source",
+        }
         widgets = {
-            'title': TextInput(attrs={'placeholder': 'i.e. Buckeyes'}),
-            'description': Textarea(attrs={'placeholder': 'i.e. Peanut butter balls coated in chocolate'}),
-            'recipe_source': Textarea(
-                attrs={'placeholder': 'i.e. Duff Bakes '
-                                      '\nor '
-                                      '\nhttps://www.amazon.com/Duff-Bakes-Think-Bake-Like/dp/0062349805'
-                                      '\nor '
-                                      '\nhttps://www.yahoo.com/entertainment/buckeyes-from-duff-bakes-1326919478968374.html'})
+            "title": TextInput(attrs={"placeholder": "i.e. Buckeyes"}),
+            "description": Textarea(
+                attrs={"placeholder": "i.e. Peanut butter balls coated in chocolate"}
+            ),
+            "recipe_source": Textarea(
+                attrs={
+                    "placeholder": "i.e. Duff Bakes "
+                    "\nor "
+                    "\nhttps://www.amazon.com/Duff-Bakes-Think-Bake-Like/dp/0062349805"
+                    "\nor "
+                    "\nhttps://www.yahoo.com/entertainment/buckeyes-from-duff-bakes-1326919478968374.html"
+                }
+            ),
         }
